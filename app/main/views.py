b9ad509cfa14ccaf5fa63aeadd66be9b72b3a2cc
Softonic254 <rotichenoch27@gmail.com>
2021-06-20 19:15:52 +0300

    It adds views function for comments that will return comment form

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from .. import db
 from ..models import Blog, Quotes
 from ..requests import get_quote
 from . import main
-from .forms import BlogForm
+from .forms import BlogForm,Comment
 
 #views
 
@@ -21,7 +21,7 @@ def index():
 
 @main.route('/home',methods=['GET','POST'])
 @login_required
-def main():
+def main_page():
     blog_form=BlogForm()
     if blog_form.validate_on_submit():
         blog_category=blog_form.blog_category.data
@@ -34,8 +34,18 @@ def main():
         db.session.add(new_blog)
         db.session.commit()
         
-        return redirect(url_for('main.main'))
+        return redirect(url_for('main.main_page'))
     else:
         blogs=Blog.query.order_by(Blog.posted).all()
-    return render_template('main/main.html',blog_form=blog_form, blogs=blogs)
+    return render_template('main_templates/main.html',blog_form=blog_form, blogs=blogs)
 
+@main.route('/home/comment/<int:id>')
+@login_required
+def comments(id):
+    comment_form=Comment()
+    if blog_form.validate_on_submit:
+        comment=comment_form.blog_comment.data
+        
+        
+        
+    return render_template('main_templates/comment.html',comment_form=comment_form)
