b25db0d6e2ba76fe84fee71f3e3d2b1fa320e044
Softonic254 <rotichenoch27@gmail.com>
2021-06-22 15:11:01 +0300

    It removes config from gitignore

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+  SQLALCHEMY_TRACK_MODIFICATIONS=True
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+
+  #Email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  
+class ProdConfig(Config):
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+  if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
+
+class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:qwerty@localhost/blog'
+  DEBUG = True
+
+
+config_options = {
+  'production':ProdConfig,
+  'development':DevConfig
+}
