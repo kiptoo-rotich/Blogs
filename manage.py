299335c247ac5fc05e64c25c95404e616dea9180
Softonic254 <rotichenoch27@gmail.com>
2021-06-22 11:02:32 +0300

    It imports config using relative path ..

--- manage.py
+++ manage.py
@@ -14,7 +14,6 @@ manager.add_command('server', Server)
 migrate=Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-
 @manager.shell
 def make_shell_context():
     return dict(app=app,db=db,User=User,Quotes=Quotes,Blog=Blog,Comment=Comment)
